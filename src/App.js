aa1e89da141bd8f10234c2e8765f0114a20ccace
pinky-banik <pinky.rani.banik@gmail.com>
2022-06-24 03:56:48 +0600

    header done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Body from './component/Body/Body';
+import Header from './component/Header/Header';
 import Navbar from './component/Navbar/Navbar';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Header/>
       <Body/>
       <Body/>
       
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import banner from '../../assets/banner.jpg'
+
 
 const Header = () => {
     return (
-        <div className="flex justify-between">
-            <div>abc</div>
-            <div>bcd</div>
+        <div className="justify-between py-10 px-3 items-center grid  md:grid-cols-2 ">
+            <div className=" justify-center items-center flex-row md:p-20 p-5">
+                <h1 className="text-2xl">Making It Expert</h1>
+                <h2 className="text-komla text-xl">Trusted organization in the world</h2>
+                <p className="text-md"> 
+                    Webtrue is by your side to keep yourself 
+                    updated with the latest technology.
+                    IT sector or non-IT sector, 
+                    there is a good demand for
+                    IT experts everywhere now. 
+                    That's why updated curriculum, 
+                    experienced mentor. 
+                    We believe that every human 
+                    being is talented, and we have a 
+                    responsibility to develop your talents. 
+                    All you need is your interest and regular practice.
+                </p>
+            </div>
+            
+            <div className="">
+                <img src={banner} alt="" />
+            </div>
         </div>
     );
 };
